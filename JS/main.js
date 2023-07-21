091128a2358ffba5948f16eb81c093206e31dc24
Xandekkj <alexandre_rodrigues2003@hotmail.com>
2023-07-21 10:09:33 -0300

    adicionando itens

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,37 @@
-document.getElementById("novoItem")
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = []
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) {
-
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = quantidade
+    novoItem.appendChild(numeroItem)
 
-    numeroItem.innerHTML = numeroItem + nome
-
-    const lista = document.getElementById("lista")
+    novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
 
-    console.log(numeroItem)
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
 }
