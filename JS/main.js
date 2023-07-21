93959cdccbfbccd40b5c880a389c2ded15e0d0d9
Xandekkj <alexandre_rodrigues2003@hotmail.com>
2023-07-21 09:58:04 -0300

    criando a estrutura

--- JS/main.js
+++ JS/main.js
@@ -8,4 +8,17 @@ form.addEventListener("submit", (evento) => {
 
 function criaElemento(nome, quantidade) {
 
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    numeroItem.innerHTML = numeroItem + nome
+
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem)
+
+    console.log(numeroItem)
 }
